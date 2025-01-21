1a404a5abca1755fd1d6da81c6267b83b6c78d53
mango2309 <sebastian.largo@udla.edu.ec>
2025-01-21 07:20:49 -0500

    Agregar archivos de proyecto.

---
+++ LargoEvaluacion3P/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LargoEvaluacion3P
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LargoEvaluacion3P/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LargoEvaluacion3P
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
